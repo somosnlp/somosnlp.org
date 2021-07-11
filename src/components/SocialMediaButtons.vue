54fde84bc8cce13a3fcdbc24e41bfe73caeeeb2b
Felix Andreas <felix.andreas95@googlemail.com>
2021-07-11 12:44:56 +0200

    feat: add slack link
    Co-authored-by: Maria Grandury <mariagrandury@gmail.com>

--- src/components/SocialMediaButtons.vue
+++ src/components/SocialMediaButtons.vue
@@ -2,10 +2,13 @@
     <LinkIconButton url="https://twitter.com/NLP_en_ES">
         <carbon-logo-twitter text="lg" />
     </LinkIconButton>
-    <LinkIconButton url="https://github.com/nlp-en-es">
-        <carbon-logo-github text="lg" />
-    </LinkIconButton>
     <LinkIconButton url="https://www.linkedin.com/company/nlp-en-es/">
         <carbon-logo-linkedin text="lg" />
+    </LinkIconButton>
+    <LinkIconButton url="https://bitly.com/nlp-en-es">
+        <carbon-logo-slack text="lg" />
+    </LinkIconButton>
+    <LinkIconButton url="https://github.com/nlp-en-es">
+        <carbon-logo-github text="lg" />
     </LinkIconButton>
 </template>
