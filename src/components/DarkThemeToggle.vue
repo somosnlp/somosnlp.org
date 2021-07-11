79fd5fabab2291856d944a990b6fd8b14a98af4b
Felix Andreas <felix.andreas95@googlemail.com>
2021-07-11 13:38:00 +0200

    feat: redesign DarkThemeToggle

--- src/components/DarkThemeToggle.vue
+++ src/components/DarkThemeToggle.vue
@@ -7,15 +7,14 @@ import { isDark } from "~/logic"
         <fluent-weather-sunny-24-regular class="cursor-pointer" @click="isDark = false" />
         <div
             @click="isDark = !isDark"
-            :class="
-                isDark ? 'border-accent-900' : ' border-accent-500'
-            "
-            class="border rounded-full cursor-pointer bg-accent-400 h-6 w-12 relative inline-flex items-start focus:outline-none"
+            class="rounded-full cursor-pointer bg-gray-100 h-6 w-12 relative inline-flex items-start focus:outline-none"
+            dark="bg-accent-400"
+            border="1 gray-300 dark:accent-900"
         >
             <span
                 :style="`transform: translate(${Number(isDark) * 24 - 1}px, -1px)`"
                 class="bg-white border rounded-full h-6 transition-transform top-0 w-6 absolute"
-                border="2 accent-600"
+                border="1 gray-300 dark:accent-800"
             ></span>
         </div>
         <fluent-weather-moon-24-regular class="cursor-pointer" @click="isDark = true" />
