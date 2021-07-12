c30a19532e10d303cfcced55e0597ed5fd79aa85
Felix Andreas <felix.andreas95@googlemail.com>
2021-07-12 21:25:12 +0200

    refactor: change background of DarkThemeToggle

--- src/components/DarkThemeToggle.vue
+++ src/components/DarkThemeToggle.vue
@@ -4,19 +4,24 @@ import { isDark } from "~/logic"
 </script>
 <template>
     <div class="flex text-gray-800 gap-2 items-center" dark="text-white">
-        <fluent-weather-sunny-24-regular class="cursor-pointer" @click="isDark = false" />
+        <carbon:moon class="cursor-pointer" @click="isDark = true" />
         <div
             @click="isDark = !isDark"
-            class="rounded-full cursor-pointer bg-gray-100 h-6 w-12 relative inline-flex items-start focus:outline-none"
-            dark="bg-accent-400"
-            border="1 gray-300 dark:accent-900"
+            class="rounded-full cursor-pointer bg-accent-300 h-6 w-12 relative"
+            dark="bg-gray-800"
+            border="1 accent-500 dark:gray-600"
         >
             <span
-                :style="`transform: translate(${Number(isDark) * 24 - 1}px, -1px)`"
+                :style="`transform: translate(${Number(!isDark) * 24 - 1}px, -1px)`"
                 class="bg-white border rounded-full h-6 transition-transform top-0 w-6 absolute"
-                border="1 gray-300 dark:accent-800"
-            ></span>
+                border="1 accent-600 dark:gray-600"
+            />
         </div>
-        <fluent-weather-moon-24-regular class="cursor-pointer" @click="isDark = true" />
+        <carbon:sun class="cursor-pointer" @click="isDark = false" />
     </div>
+    <!-- Alternative DarkThemeToggle -->
+    <!-- <IconButton @click="isDark = !isDark">
+        <carbon:sun v-if="!isDark" class="cursor-pointer" />
+        <carbon:moon v-else class="cursor-pointer" />
+    </IconButton>-->
 </template>
