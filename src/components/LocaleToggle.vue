4ba9bce58452900e2348b6954d4835de2bd54584
Felix Andreas <felix.andreas95@googlemail.com>
2021-07-12 18:17:07 +0200

    refactor: simplify LocaleToggleButton

--- src/components/LocaleToggle.vue
+++ src/components/LocaleToggle.vue
@@ -1,26 +1,21 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 const { locale } = useI18n()
-
-const setSpanish = () => { locale.value = 'es' }
-const setEnglish = () => { locale.value = 'en' }
 </script>
 
 <template>
-    <!-- <div @click="setSpanish">Spanish</div> -->
     <div class="flex gap-2">
-        <IconButton @click="setSpanish">
+        <IconButton @click="locale = 'es'">
             <div class="flex font-medium text-sm gap-1 items-center">
                 <twemoji:flag-for-flag-spain />
                 <span>ES</span>
             </div>
         </IconButton>
-        <IconButton @click="setEnglish">
+        <IconButton @click="locale = 'en'">
             <div class="flex font-medium text-sm gap-1 items-center">
                 <twemoji:flag-for-flag-united-kingdom />
                 <span>EN</span>
             </div>
         </IconButton>
     </div>
-    <!-- <div @click="setEnglish">English</div> -->
 </template>
