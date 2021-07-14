d43675598959b4804e88bdade2ef98820c4575df
Felix Andreas <felix.andreas95@googlemail.com>
2021-07-15 00:57:32 +0200

    feat: change scrollbehavior

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,16 @@ import 'virtual:windi.css'
 import 'virtual:windi-devtools'
 import './styles/main.css'
 
+import type { RouterScrollBehavior } from 'vue-router'
+
+const scrollBehavior: RouterScrollBehavior = (to, from, savedPosition) => {
+  return savedPosition ? savedPosition : { top: 0 }
+}
 
 // https://github.com/antfu/vite-ssg
 export const createApp = ViteSSG(
   App,
-  { routes },
+  { routes, scrollBehavior },
   (ctx) => {
     // install all modules under `modules/`
     Object.values(import.meta.globEager('./modules/*.ts')).map(i => i.install?.(ctx))
