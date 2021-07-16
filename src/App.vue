543237ab9621192685c10f48d2c7fcc30bfff0e4
Felix Andreas <felix.andreas95@googlemail.com>
2021-07-16 13:02:56 +0200

    feat: add og description

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ useHead({
 
     },
     { property: 'og:title', content: 'NLP en ES' },
+    { property: 'og:description', content: 'Comunidad de profesionales, investigadores y estudiantes acelerando el avance del NLP en Español.' },
     { property: 'og:image', content: 'https://nlp-en-es.github.io/assets/logo.png' },
     { name: 'twitter:card', content: 'summary' },
     { name: 'twitter:creator', content: '@NLP_en_ES' },
