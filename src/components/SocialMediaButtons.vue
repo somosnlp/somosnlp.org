20516e2b7282cc12a26f73b72833d6b0a9c52707
Felix Andreas <felix.andreas95@googlemail.com>
2021-07-16 19:17:44 +0200

    refactor: use colon in SocialMediaButtons

--- src/components/SocialMediaButtons.vue
+++ src/components/SocialMediaButtons.vue
@@ -1,14 +1,14 @@
 <template>
     <LinkIconButton url="https://twitter.com/NLP_en_ES">
-        <carbon-logo-twitter />
+        <carbon:logo-twitter />
     </LinkIconButton>
     <LinkIconButton url="https://www.linkedin.com/company/nlp-en-es/">
-        <carbon-logo-linkedin />
+        <carbon:logo-linkedin />
     </LinkIconButton>
     <LinkIconButton url="https://bitly.com/nlp-en-es">
-        <carbon-logo-slack />
+        <carbon:logo-slack />
     </LinkIconButton>
     <LinkIconButton url="https://github.com/nlp-en-es">
-        <carbon-logo-github />
+        <carbon:logo-github />
     </LinkIconButton>
 </template>
