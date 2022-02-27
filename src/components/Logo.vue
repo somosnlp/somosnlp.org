a5280097b9c3f9d475cf7a75343f9950577a1330
María <mariagrandury@gmail.com>
2022-02-27 20:11:33 +0100

    feat: update favicon and logo mark

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -2,7 +2,6 @@
     <router-link to="/">
         <div class="flex gap-4 items-center">
             <LogoMark />
-            <!-- <carbon:earth text="2xl" /> -->
             <div class="font-semibold text-xl whitespace-nowrap">Somos NLP</div>
         </div>
     </router-link>
