98d6b46ef627743e7913c60208dc1c5712de7dad
Maria Grandury <mariagrandury@gmail.com>
2022-08-31 10:46:16 +0200

    blog: add video tutorials to resources

--- src/components/ResourcesList.vue
+++ src/components/ResourcesList.vue
@@ -33,22 +33,25 @@ const routes_notebooks = router.getRoutes()
     </header>
 
     <hr class="mx-auto mt-8 mb-12 prose" />
-
     <div class="auto-rows-fr grid gap-2 lg:grid-cols-2">
         <BlogItem v-for="route in routes_open_source" :key="route.path" :route="route" />
     </div>
+    <div class="my-12 flex justify-center">
+        <img src="https://somosnlp.github.io/assets/images/undraw_education_edited.svg" width="300" height="365" />
+    </div>
 
     <hr class="mx-auto mt-12 mb-12 prose" />
-
     <h2 class="text-3xl font-bold m-auto text-center prose">Tutoriales</h2>
     <div class="auto-rows-fr grid gap-2 lg:grid-cols-2">
         <BlogItem v-for="route in routes_notebooks" :key="route.path" :route="route" />
     </div>
+    <div class="mx-auto text-center">
+        <a href="https://youtube.com/c/somosnlp" target="_blank" class="button-accent">
+            <carbon:logo-youtube />
+            Vídeo Tutoriales
+        </a>
+    </div>
     <div class="my-12 flex justify-center">
-        <img
-            src="https://somosnlp.github.io/assets/images/undraw_education_edited.svg"
-            width="300"
-            height="365"
-        />
+        <img src="undraw_video_upload_3d4u.svg" width="300" height="365" />
     </div>
 </template>
