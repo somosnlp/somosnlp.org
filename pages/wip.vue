98cb9e9d2195680bb6477d425bfef4fa1544000e
Maria Grandury <mariagrandury@gmail.com>
2022-08-31 22:03:34 +0200

    fix: wip import statements

--- pages/wip.vue
+++ pages/wip.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+</script>
+
 <template>
       <Container>
             <div class="mx-auto my-16 text-center max-w-400px grid text-2xl gap-4 place-items-center">
