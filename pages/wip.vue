50e42683eca42d58b350a97e69ee16d83c690a9a
Maria Grandury <mariagrandury@gmail.com>
2022-08-31 12:37:06 +0200

    feat: add WIP text to yml files

--- pages/wip.vue
+++ pages/wip.vue
@@ -1,14 +1,10 @@
 <template>
       <Container>
-            <div
-                  class="mx-auto my-16 text-center max-w-400px grid text-2xl gap-4 place-items-center"
-            >
-                  <img
-                        src="https://somosnlp.github.io/assets/images/undraw_good_team_re_j1kc.svg"
-                        alt="Trabajando..."
-                  />
+            <div class="mx-auto my-16 text-center max-w-400px grid text-2xl gap-4 place-items-center">
+                  <img src="https://somosnlp.github.io/assets/images/undraw_good_team_re_j1kc.svg"
+                        alt="Work In Progress..." />
                   <div class="font-bold tracking-wide text-4xl">404</div>
-                  <div class="tracking-tight">Estamos trabajando en ello</div>
+                  <div class="tracking-tight">{{  t('wip')  }}</div>
             </div>
       </Container>
 </template>
