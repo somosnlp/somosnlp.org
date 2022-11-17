4a645defe3d87c9e24eb2362fbea578dd1599e4a
Maria Grandury <mariagrandury@gmail.com>
2022-11-17 13:48:17 +0100

    content: update WIP message

--- pages/wip.vue
+++ pages/wip.vue
@@ -5,11 +5,11 @@ const { t } = useI18n()
 
 <template>
       <Container>
-            <div class="mx-auto my-16 text-center max-w-400px grid text-2xl gap-4 place-items-center">
+            <div class="mx-auto my-16 text-center max-w-400px grid gap-4 place-items-center">
                   <img src="https://somosnlp.github.io/assets/images/undraw_good_team_re_j1kc.svg"
                         alt="Work In Progress..." />
                   <div class="font-bold tracking-wide text-4xl">404</div>
-                  <div class="tracking-tight">{{  t('wip')  }}</div>
+                  <div class="tracking-tight text-lg">{{ t('wip') }}</div>
             </div>
       </Container>
 </template>
