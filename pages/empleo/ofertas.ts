4ef827a6b3ae0cd3f968387c2a43c5a924cae219
Maria Grandury <mariagrandury@gmail.com>
2023-01-20 16:28:33 +0100

    content: publicar ofertas de empleo

--- pages/empleo/ofertas.ts
+++ pages/empleo/ofertas.ts
@@ -1,40 +1,76 @@
-const somos_nlp_data_scientist = {
-    ocupación: 'Data Scientist',
-    "palabras clave": 'traducción',
-    contrato: 'T. Completo',
-    entidad: 'Somos NLP',
-    "nº trabajadores/as": '0 - 9',
-    localización: 'Remoto 100%',
-    "fecha publicación": '2022/09/09',
-    más: 'https://somosnlp.org'
-}
-
-const somos_nlp_nlp_engineer = {
-    ocupación: 'NLP Engineer',
-    "palabras clave": 'traducción',
-    contrato: 'T. Parcial',
-    entidad: 'Somos NLP',
+const siris_nlp_researcher = {
+    ocupación: 'NLP Researcher, Data Scientist',
+    "palabras clave": '',
+    contrato: 'T.Completo',
+    entidad: 'SIRIS Academic',
     "nº trabajadores/as": '10 - 49',
-    localización: 'Presencial Madrid',
-    "fecha publicación": '2022/10/01',
-    más: 'https://somosnlp.org/blog'
+    localización: 'Presencial/Híbrido Barcelona',
+    "fecha publicación": '2022/11/10',
+    más: 'https://sirisacademic.com/we-are-hiring/data-scientist-researcher/'
 }
 
-const somos_nlp_volunteer = {
-    ocupación: 'Volunteer',
-    "palabras clave": 'traducción',
-    contrato: 'Prácticas',
-    entidad: 'Somos NLP',
+const bsc_data_engineer = {
+    ocupación: 'Data Engineer for Language and Translation Technologies (RE1)',
+    "palabras clave": 'NLP, ML, Data',
+    contrato: 'T.Completo',
+    entidad: 'Barcelona Supercomputing Center',
     "nº trabajadores/as": 'Más de 250',
-    localización: 'Híbrido Alicante',
-    "fecha publicación": '2022/07/01',
-    más: 'https://somosnlp.org/hackathon'
+    localización: 'Híbrido Barcelona',
+    "fecha publicación": '2023/01/18',
+    más: 'https://www.bsc.es/join-us/job-opportunities/923lstmre1'
+}
+
+const bsc_machine_translation_engineer_re1 = {
+    ocupación: 'Machine Translation Engineer (RE1)',
+    "palabras clave": 'Machine Translation',
+    contrato: 'T.Completo',
+    entidad: 'Barcelona Supercomputing Center',
+    "nº trabajadores/as": 'Más de 250',
+    localización: 'Híbrido Barcelona',
+    "fecha publicación": '2023/01/19',
+    más: 'https://www.bsc.es/join-us/job-opportunities/1023lstmre1'
+}
+
+const bsc_machine_translation_engineer_re2 = {
+    ocupación: 'Machine Translation Engineer (RE2)',
+    "palabras clave": 'DL, NLP',
+    contrato: 'T.Completo',
+    entidad: 'Barcelona Supercomputing Center',
+    "nº trabajadores/as": 'Más de 250',
+    localización: 'Híbrido Barcelona',
+    "fecha publicación": '2023/01/19',
+    más: 'https://www.bsc.es/join-us/job-opportunities/1323lstmre2'
+}
+
+const bsc_deep_learning_engineer = {
+    ocupación: 'Deep Learning Engineer for Language Technologies (RE2)',
+    "palabras clave": 'DL, NLP',
+    contrato: 'T.Completo',
+    entidad: 'Barcelona Supercomputing Center',
+    "nº trabajadores/as": 'Más de 250',
+    localización: 'Híbrido Barcelona',
+    "fecha publicación": '2023/01/19',
+    más: 'https://www.bsc.es/join-us/job-opportunities/1423lstmre2'
+}
+
+const tirant_lo_blanch_nlp_engineer = {
+    ocupación: 'NLP Engineer',
+    "palabras clave": 'NLP, Transformers, Legal',
+    contrato: 'T.Completo',
+    entidad: 'Tirant lo Blanch',
+    "nº trabajadores/as": 'Más de 250',
+    localización: 'Remoto 100% (Zona horaria GMT+1)',
+    "fecha publicación": '2023/01/19',
+    más: 'https://tirant.com/trabaja-con-nosotros/noticia-descripcion-data-engineer/'
 }
 
 export const columnas = ['ocupación', 'contrato', 'entidad', 'nº trabajadores/as', 'localización', 'fecha publicación', 'más']
 
 export const ofertas = [
-    somos_nlp_data_scientist,
-    somos_nlp_nlp_engineer,
-    somos_nlp_volunteer,
+    siris_nlp_researcher,
+    bsc_data_engineer,
+    bsc_machine_translation_engineer_re1,
+    bsc_machine_translation_engineer_re2,
+    bsc_deep_learning_engineer,
+    tirant_lo_blanch_nlp_engineer
 ]
