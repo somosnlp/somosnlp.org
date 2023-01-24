fadc53cfca9dd03db8dc020585668b339c1c624b
Maria Grandury <mariagrandury@gmail.com>
2023-01-24 20:46:46 +0100

    content: add new job offers
    - Add new job offers
    - Add key words
    - Add application deadlines

--- pages/empleo/ofertas.ts
+++ pages/empleo/ofertas.ts
@@ -11,57 +11,97 @@ const siris_nlp_researcher = {
 
 const bsc_data_engineer = {
     ocupación: 'Data Engineer for Language and Translation Technologies (RE1)',
-    "palabras clave": 'NLP, ML, Data',
+    "palabras clave": 'BSC',
     contrato: 'T.Completo',
     entidad: 'Barcelona Supercomputing Center',
     "nº trabajadores/as": 'Más de 250',
     localización: 'Híbrido Barcelona',
     "fecha publicación": '2023/01/18',
+    "fecha límite": '2023/02/28',
     más: 'https://www.bsc.es/join-us/job-opportunities/923lstmre1'
 }
 
 const bsc_machine_translation_engineer_re1 = {
     ocupación: 'Machine Translation Engineer (RE1)',
-    "palabras clave": 'Machine Translation',
+    "palabras clave": 'BSC, Traducción, Traduccion',
     contrato: 'T.Completo',
     entidad: 'Barcelona Supercomputing Center',
     "nº trabajadores/as": 'Más de 250',
     localización: 'Híbrido Barcelona',
     "fecha publicación": '2023/01/19',
+    "fecha límite": '2023/02/28',
     más: 'https://www.bsc.es/join-us/job-opportunities/1023lstmre1'
 }
 
 const bsc_machine_translation_engineer_re2 = {
     ocupación: 'Machine Translation Engineer (RE2)',
-    "palabras clave": 'DL, NLP',
+    "palabras clave": 'BSC, DL, Traducción, Traduccion',
     contrato: 'T.Completo',
     entidad: 'Barcelona Supercomputing Center',
     "nº trabajadores/as": 'Más de 250',
     localización: 'Híbrido Barcelona',
     "fecha publicación": '2023/01/19',
+    "fecha límite": '2023/02/28',
     más: 'https://www.bsc.es/join-us/job-opportunities/1323lstmre2'
-}
-
-const bsc_deep_learning_engineer = {
-    ocupación: 'Deep Learning Engineer for Language Technologies (RE2)',
-    "palabras clave": 'DL, NLP',
-    contrato: 'T.Completo',
-    entidad: 'Barcelona Supercomputing Center',
-    "nº trabajadores/as": 'Más de 250',
-    localización: 'Híbrido Barcelona',
-    "fecha publicación": '2023/01/19',
-    más: 'https://www.bsc.es/join-us/job-opportunities/1423lstmre2'
 }
 
 const tirant_lo_blanch_nlp_engineer = {
     ocupación: 'NLP Engineer',
-    "palabras clave": 'NLP, Transformers, Legal',
+    "palabras clave": 'Transformers, Legal',
     contrato: 'T.Completo',
     entidad: 'Tirant lo Blanch',
     "nº trabajadores/as": 'Más de 250',
     localización: 'Remoto 100% (Zona horaria GMT+1)',
     "fecha publicación": '2023/01/19',
     más: 'https://tirant.com/trabaja-con-nosotros/noticia-descripcion-data-engineer/'
+}
+
+const bsc_deep_learning_engineer_language_re2 = {
+    ocupación: 'Deep Learning Engineer for Language Technologies (RE2)',
+    "palabras clave": 'BSC, DL',
+    contrato: 'T.Completo',
+    entidad: 'Barcelona Supercomputing Center',
+    "nº trabajadores/as": 'Más de 250',
+    localización: 'Híbrido Barcelona',
+    "fecha publicación": '2023/01/19',
+    "fecha límite": '2023/02/28',
+    más: 'https://www.bsc.es/join-us/job-opportunities/1423lstmre2'
+}
+
+const bsc_deep_learning_engineer_language_re1 = {
+    ocupación: 'Deep Learning Engineer for Language Technologies (RE1)',
+    "palabras clave": 'BSC, Traducción, MLOps',
+    contrato: 'T.Completo',
+    entidad: 'Barcelona Supercomputing Center',
+    "nº trabajadores/as": 'Más de 250',
+    localización: 'Híbrido Barcelona',
+    "fecha publicación": '2023/01/24',
+    "fecha límite": '2023/02/28',
+    más: 'https://www.bsc.es/join-us/job-opportunities/1123lstmre1'
+}
+
+const bsc_deep_learning_engineer_speech_re1 = {
+    ocupación: 'Deep Learning Engineer for Speech Technologies (RE1)',
+    "palabras clave": 'BSC, DL, Speech, Voz',
+    contrato: 'T.Completo',
+    entidad: 'Barcelona Supercomputing Center',
+    "nº trabajadores/as": 'Más de 250',
+    localización: 'Híbrido Barcelona',
+    "fecha publicación": '2023/01/25',
+    "fecha límite": '2023/03/16',
+    más: 'https://www.bsc.es/join-us/job-opportunities/3023lstmre1'
+}
+
+const bsc_deep_learning_engineer_speech_re2 = {
+    ocupación: 'Deep Learning Engineer for Speech Technologies (RE2)',
+    "palabras clave": 'BSC, DL, Speech, Voz',
+    contrato: 'T.Completo',
+    entidad: 'Barcelona Supercomputing Center',
+    "nº trabajadores/as": 'Más de 250',
+    localización: 'Híbrido Barcelona',
+    "fecha publicación": '2023/01/25',
+    "fecha límite": '2023/03/16',
+    más: 'https://www.bsc.es/join-us/job-opportunities/3323lstmre2'
 }
 
 export const columnas = ['ocupación', 'contrato', 'entidad', 'nº trabajadores/as', 'localización', 'fecha publicación', 'más']
@@ -71,6 +111,9 @@ export const ofertas = [
     bsc_data_engineer,
     bsc_machine_translation_engineer_re1,
     bsc_machine_translation_engineer_re2,
-    bsc_deep_learning_engineer,
-    tirant_lo_blanch_nlp_engineer
+    tirant_lo_blanch_nlp_engineer,
+    bsc_deep_learning_engineer_language_re2,
+    bsc_deep_learning_engineer_language_re1,
+    bsc_deep_learning_engineer_speech_re1,
+    bsc_deep_learning_engineer_speech_re2,
 ]
