55a93e3717137cc05ff478056d2a76fa6033a0b7
Maria Grandury <mariagrandury@gmail.com>
2023-02-26 00:17:59 +0100

    feat: crear sección eventos

---
+++ pages/eventos.vue
@@ -0,0 +1,150 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <Container class="my-12">
+
+        <header class="m-auto text-center prose">
+            <h1>Eventos</h1>
+            <p class="text-lg">
+                Charlas y talleres impartidos por especialistas del campo del NLP.
+            </p>
+        </header>
+
+        <hr class="mx-auto mt-8 mb-12 prose" />
+
+        <article class="m-auto prose">
+            <div class="m-12">
+                <ul>
+                    <li>
+                        <a href="#proyectos-de-la-comunidad">Proyectos de la comunidad</a>
+                    </li>
+                    <li>
+                        <a href="#hackathon-2022">Charlas y talleres del Hackathon 2022</a>
+                    </li>
+                </ul>
+        </div>
+
+        <!-- <div class="mt-10 grid gap-4 lg:grid-cols-2">
+                <a href="https://discord.com/invite/eUEkKpSw7d" target="_blank" class="button-accent">
+                        <Carbon:logoYoutube />YouTube
+                    </a>
+                    <router-link to="/nuestra-mision" class="button-gray">
+                                <Carbon:binoculars />EventBrite
+                            </router-link>
+                        </div> -->
+
+            <hr class="mx-auto mt-8 mb-12 prose" />
+
+            <h2 id="proyectos-de-la-comunidad">🙌 Proyectos de la comunidad</h2>
+
+            <div class="mx-auto my-8 text-center">
+                <div class="grid grid-cols-2 gap-8 my-1">
+                    <a href="https://www.youtube.com/watch?v=fOQLPuXewzE&list=PLTA-KAy8nxaAbyaBTYK68TZKQLv9V8L8M"
+                        target="_blank">
+                        <img alt="BiomedIA" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/eventos/230112_biomedia.jpg" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=XOaQKNauySo&list=PLTA-KAy8nxaAbyaBTYK68TZKQLv9V8L8M"
+                        target="_blank">
+                        <img alt="Modelo Jurídico Mexicano" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/eventos/221213_neutralizacion_de_genero.jpg" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=ZTYAsEHUhPs&list=PLTA-KAy8nxaAbyaBTYK68TZKQLv9V8L8M"
+                        target="_blank">
+                        <img alt="Neutralización de Género" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/eventos/221208_modelo_juridico_mexicano.jpg" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=1LmH6lblxvg&list=PLTA-KAy8nxaAbyaBTYK68TZKQLv9V8L8M"
+                        target="_blank">
+                        <img alt="Detector de Sexismo" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/eventos/221212_detector_sexismo.jpg" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=t86nV8Vfzbg&list=PLTA-KAy8nxaAbyaBTYK68TZKQLv9V8L8M"
+                        target="_blank">
+                        <img alt="Validador de Veracidad de Noticias" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/eventos/221129_news_veracity_validator.png" />
+                    </a>
+                </div>
+            </div>
+
+            <h2 id="hackathon-2022">🚀 Charlas y talleres del Hackathon 2022</h2>
+
+            <div class="mx-auto my-8 text-center">
+                <div class="grid grid-cols-2 gap-8 my-1">
+                    <a href="https://www.youtube.com/watch?v=3OhArr1R2Lw&list=PLTA-KAy8nxaAbVZ2lVcycHnJ2qEDip7hG"
+                        target="_blank">
+                        <img alt="RigoBERTa" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/evento_iic.png" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=GX4l3WhOy4o&list=PLTA-KAy8nxaAbVZ2lVcycHnJ2qEDip7hG"
+                        target="_blank">
+                        <img alt="IA y NLP, una apasionante encrucijada" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/evento_cristina.png" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=UQwWTykNFW0&list=PLTA-KAy8nxaAbVZ2lVcycHnJ2qEDip7hG"
+                        target="_blank">
+                        <img alt="Muestreo de datos BERTIN" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/evento_paulo.png" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=aNR7UM-E6vA&list=PLTA-KAy8nxaAbVZ2lVcycHnJ2qEDip7hG"
+                        target="_blank">
+                        <img alt="Lenguas Minorizadas - MX" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/evento_ximena.png" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=_TbNgSodiPY&list=PLTA-KAy8nxaAbVZ2lVcycHnJ2qEDip7hG"
+                        target="_blank">
+                        <img alt="El ecosistema de Hugging Face" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/evento_14_03_ecosistema_hf.png" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=jvhEra9mdPw&list=PLTA-KAy8nxaAbVZ2lVcycHnJ2qEDip7hG"
+                        target="_blank">
+                        <img alt="Análisis del error" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/evento_16_03_errores.png" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=_f3eLbrFiCc&list=PLTA-KAy8nxaAbVZ2lVcycHnJ2qEDip7hG"
+                        target="_blank">
+                        <img alt="AMA I" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/evento_17_03_ama.png" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=UR6MHBbPA3g&list=PLTA-KAy8nxaAbVZ2lVcycHnJ2qEDip7hG"
+                        target="_blank">
+                        <img alt="Etiquetado de datos" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/evento_29_03_anotacion.png" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=j4qfRljYNYI&list=PLTA-KAy8nxaAbVZ2lVcycHnJ2qEDip7hG"
+                        target="_blank">
+                        <img alt="Modelos del Lenguaje para RRSS" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/evento_22_03_cardiff.png" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=RWIZj9puFX0&list=PLTA-KAy8nxaAbVZ2lVcycHnJ2qEDip7hG"
+                        target="_blank">
+                        <img alt="AMA II" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/evento_ama_lewis.png" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=U8fig2fqrl8&list=PLTA-KAy8nxaAbVZ2lVcycHnJ2qEDip7hG"
+                        target="_blank">
+                        <img alt="Traducción Automática" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/evento_eva.png" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=7ckg10QUHLw&list=PLTA-KAy8nxaAbVZ2lVcycHnJ2qEDip7hG"
+                        target="_blank">
+                        <img alt="Topics con clustering no-supervisado" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/evento_31_03_topics.png" />
+                    </a>
+                    <a href="https://www.youtube.com/watch?v=QziYfITvGrA&list=PLTA-KAy8nxaAbVZ2lVcycHnJ2qEDip7hG"
+                        target="_blank">
+                        <img alt="Taller de escritura de abstracts" width="650" height="365"
+                            src="https://somosnlp.github.io/assets/images/evento_abstracts.png" />
+                    </a>
+                </div>
+            </div>
+
+            <a href="https://www.youtube.com/c/somosnlp?sub_confirmation=1" target="_blank"
+                class="button-accent max-w-100 mx-auto">
+                <carbon:logo-youtube text="xl" />
+                <span text="center">Ver todos los eventos</span>
+            </a>
+        </article>
+    </Container>
+</template>
