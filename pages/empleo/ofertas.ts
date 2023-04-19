5639f35330770b2fca1b0a63fbb130496012c3d7
Oscar M. Cumbicus-Pineda <61592750+oskrmiguel@users.noreply.github.com>
2023-04-19 13:38:16 -0500

    Update ofertas.ts (#118)
    empleo: NLP Engineer en HITZ
    
    ---------
    
    Co-authored-by: María Grandury <57645283+mariagrandury@users.noreply.github.com>

--- pages/empleo/ofertas.ts
+++ pages/empleo/ofertas.ts
@@ -63,7 +63,16 @@ const bsc_machine_translation_engineer_re1 = {
     "fecha publicación (fecha límite)": '2023/01/19 (2023/03/31)',
     más: 'https://www.bsc.es/join-us/job-opportunities/1023lstmre1'
 }
-
+const NLP_Engineer_Hitz = {
+    ocupación: 'NLP Engineer',
+    "palabras clave": 'Hitz, NLP, LLM',
+    contrato: 'T.Completo',
+    entidad: 'Hitz Center for Language Technology, University of the Basque Country (UPV/EHU)',
+    "nº trabajadores/as": 'Más de 250',
+    localización: 'San Sebastian, Bilbao. Mínimo 30% presencial',
+    "fecha publicación (fecha límite)": '2023/04/19 (2023/12/31)',
+    más: 'http://www.hitz.eus/en/job-offers'
+}
 export const columnas = ['ocupación', 'contrato', 'entidad', 'nº trabajadores/as', 'localización', 'fecha publicación (fecha límite)', 'más']
 
 export const ofertas = [
@@ -73,4 +82,5 @@ export const ofertas = [
     bsc_deep_learning_engineer_language_re2,
     tirant_lo_blanch_nlp_engineer,
     bsc_machine_translation_engineer_re1,
+    NLP_Engineer_Hitz,
 ]
