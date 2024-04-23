05f18398b0ac289faaf15fc0e2398e11a1487e5a
María Grandury <mariagrandury@gmail.com>
2024-04-23 23:11:29 +0200

    chore: remove vite-plugin-pwa
    The website does not automatically update and this is causing a lot of confusion among users

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
 import ViteComponents from 'vite-plugin-components'
 import Markdown from 'vite-plugin-md'
 import WindiCSS from 'vite-plugin-windicss'
-import { VitePWA } from 'vite-plugin-pwa'
 import VueI18n from '@intlify/vite-plugin-vue-i18n'
 import Prism from 'markdown-it-prism'
 import matter from 'gray-matter'
@@ -85,39 +84,6 @@ export default defineConfig({
     // https://github.com/antfu/vite-plugin-windicss
     WindiCSS({
       safelist: 'prose prose-sm m-auto text-left',
-    }),
-
-    // https://github.com/antfu/vite-plugin-pwa
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
-      workbox: { // fix bug as stated in https://vite-pwa-org.netlify.app/guide/#configuring-vite-plugin-pwa
-        clientsClaim: true,
-        skipWaiting: true
-      },
-      manifest: {
-        name: 'SomosNLP',
-        short_name: 'SomosNLP',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-      },
     }),
 
     // https://github.com/intlify/vite-plugin-vue-i18n
