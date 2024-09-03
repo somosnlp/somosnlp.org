6424fb01406cb653480b5888c4324be013db2042
María Grandury <mariagrandury@gmail.com>
2024-09-03 19:45:58 +0200

    add link to hf org

--- src/components/SocialMediaButtonsAll.vue
+++ src/components/SocialMediaButtonsAll.vue
@@ -8,6 +8,12 @@
     <IconButtonLink url="https://discord.com/invite/my8w7JUxZR">
         <carbon:logo-discord />
     </IconButtonLink>
+    <IconButtonLink url="https://github.com/somosnlp">
+        <carbon:logo-github />
+    </IconButtonLink>
+    <IconButtonLink url="https://somosnlp.eventbrite.com">
+        <gg:eventbrite />
+    </IconButtonLink>
     <IconButtonLink url="https://www.youtube.com/c/somosnlp?sub_confirmation=1">
         <carbon:logo-youtube />
     </IconButtonLink>
