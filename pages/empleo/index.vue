ffad3e2ba51b338433a0895f2fb0512e07e59003
María Grandury <mariagrandury@gmail.com>
2025-01-17 17:47:46 +0100

    feat: translate job board

--- pages/empleo/index.vue
+++ pages/empleo/index.vue
@@ -24,15 +24,15 @@ useHead({
 <template>
     <Container class="my-12">
         <header class="m-auto text-center prose">
-            <h1>Bolsa de Empleo de PLN</h1>
+            <h1>{{ t('jobs.title') }}</h1>
             <p class="text-lg">
-                Encuentra ofertas de empleo e investigación relacionadas con el Procesamiento del Lenguaje Natural 🚀
+                {{ t('jobs.subtitle') }} 🚀
             </p>
             <div class="justify-center items-center mx-65">
                 <IconButtonLink
                     :url='`https://twitter.com/intent/tweet?text=¡Nuevas ofertas en la bolsa de empleo de @SomosNLP_! \n\n\n Descúbrelas aquí: https://somosnlp.org/empleo`'>
                     <carbon:logo-twitter class="inline align-middle mr-2 text-lg" />
-                    <span class="font-medium text-sm">Compartir</span>
+                    <span class="font-medium text-sm">{{ t('share') }}</span>
                 </IconButtonLink>
             </div>
         </header>
@@ -48,35 +48,33 @@ useHead({
         </div>
         <div class="mt-8 mb-12">
             <p class="m-auto text-center prose italic">
-                Las entidades son responsables de publicar información correcta y actualizarla.
+                {{ t('jobs.disclaimer') }}
             </p>
         </div>
         <hr class="mx-auto mt-8 mb-12 prose" />
         <div>
             <p class="m-auto text-center prose text-2xl">
-                👏 Total Ofertas Publicadas: 51 👏
+                👏 {{ t('jobs.total') }} - 51 👏
             </p>
         </div>
         <hr class="mx-auto mt-8 mb-12 prose" />
         <div class="grid grid-cols-2">
             <div>
-                <p class="m-auto text-center prose text-2xl">BUSCO EMPLEO</p>
-                <p class="m-auto text-center prose">¡Síguenos! Anunciamos las ofertas en</p>
+                <p class="m-auto text-center prose text-2xl">{{ t('jobs.looking_for_job') }}</p>
+                <p class="m-auto text-center prose">{{ t('jobs.follow_us') }}</p>
                 <p class="m-auto text-center prose">
                     <a href="https://twitter.com/SomosNLP_" target="_blank">Twitter</a>,
-                    <a href="https://www.linkedin.com/company/somosnlp" target="_blank">LinkedIn</a> y el canal #empleo
-                    de
+                    <a href="https://www.linkedin.com/company/somosnlp" target="_blank">LinkedIn</a> {{ t('jobs.and_discord') }}
                     <a href="https://discord.com/invite/my8w7JUxZR" target="_blank">Discord</a> ✅
                 </p>
             </div>
             <div>
-                <p class="m-auto text-center prose text-2xl">BUSCO TALENTO</p>
+                <p class="m-auto text-center prose text-2xl">{{ t('jobs.looking_for_talent') }}</p>
                 <p class="m-auto text-center prose">
-                    Para que aparezca aquí tu oferta de empleo
+                    {{ t('jobs.post_job_1') }}
                 </p>
                 <p class="m-auto text-center prose">
-                    <a class="contents" href="https://forms.gle/H4LenjP1FGWgE6r4A" target="_blank">rellena este
-                        formulario</a> 📝
+                    <a class="contents" href="https://forms.gle/H4LenjP1FGWgE6r4A" target="_blank">{{ t('jobs.post_job_2') }}</a> 📝
                 </p>
             </div>
         </div>
