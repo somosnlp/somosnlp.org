c74f864b6ea2f2a49bec079543b533964e66e900
María Grandury <mariagrandury@gmail.com>
2025-03-27 16:56:06 -0300

    feat: automatizar actualización bolsa de empleo

--- pages/empleo/ofertas.ts
+++ pages/empleo/ofertas.ts
@@ -1,55 +1,30 @@
 // Para añadir una nueva oferta, crea una constante con la información correspondiente y añádela a la lista `ofertas` al final del archivo.
 
-const beluck = {
-    ocupación: 'NLP Engineer',
-    "palabras clave": 'LLM, Pytorch, embeddings',
-    contrato: 'T.Completo',
-    entidad: 'beLuck',
-    "nº trabajadores/as": '50-249',
-    localización: 'Presencial en Asturias',
-    "fecha publicación (fecha límite)": '2024/07/26 (2024/08/31)',
-    más: 'https://www.beluck.es/trabajos/experto-a-en-programacion-de-lenguaje-natural-pln'
+const orga_ai_machine_learning_researcher = {
+    "ocupación": "Machine Learning Researcher",
+    "palabras clave": "",
+    "contrato": "Tiempo Completo",
+    "entidad": "Orga AI",
+    "nº trabajadores/as": "0 - 9",
+    "localización": "Remoto, España (Sede en Palma de Mallorca)",
+    "fecha publicación (fecha límite)": "2025/02/19",
+    "más": "https://www.linkedin.com/jobs/view/4153212079/"
 }
 
-const all_jobs_bsc = {
-    ocupación: 'Ofertas de investigación',
-    "palabras clave": '',
-    contrato: 'T.Completo',
-    entidad: 'Barcelona Supercomputing Center (BSC)',
-    "nº trabajadores/as": 'Más de 250',
-    localización: 'Híbrido, Plaça Eusebi Güell, 1-3, 08034, Barcelona',
-    "fecha publicación (fecha límite)": '2024/06/01 (2024/07/31)',
-    más: 'https://www.bsc.es/join-us/job-opportunities'
+const trueflag_principal_nlp_engineer = {
+    "ocupación": "Principal NLP Engineer",
+    "palabras clave": "LLMs, Tranformers",
+    "contrato": "Tiempo Completo",
+    "entidad": "Trueflag",
+    "nº trabajadores/as": "10 - 49",
+    "localización": "Remoto desde España",
+    "fecha publicación (fecha límite)": "2025/03/10 (2025/05/05)",
+    "más": "https://www.linkedin.com/jobs/view/4181079020"
 }
-
-const all_jobs_hitz = {
-    ocupación: 'Ofertas de investigación',
-    "palabras clave": '',
-    contrato: 'T.Completo',
-    entidad: 'Hitz Center for Language Technology, University of the Basque Country (UPV/EHU)',
-    "nº trabajadores/as": 'Más de 250',
-    localización: 'San Sebastian, Bilbao. Mínimo 30% presencial',
-    "fecha publicación (fecha límite)": '2024/06/01',
-    más: 'https://www.hitz.eus/job-offers'
-}
-
-const all_jobs_iic = {
-    ocupación: 'Ofertas de empleo',
-    "palabras clave": '',
-    contrato: 'T.Completo',
-    entidad: 'Instituto de Ingeniería del Conocimiento (IIC)',
-    "nº trabajadores/as": 'Más de 250',
-    localización: 'Campus de Cantoblanco de la UAM, Madrid',
-    "fecha publicación (fecha límite)": '2024/06/01',
-    más: 'https://www.iic.uam.es/empleo-iic/ofertas'
-}
-
 
 export const columnas = ['ocupación', 'contrato', 'entidad', 'nº trabajadores/as', 'localización', 'fecha publicación (fecha límite)', 'más']
 
 export const ofertas = [
-    beluck,
-    all_jobs_bsc,
-    all_jobs_hitz,
-    all_jobs_iic
+    orga_ai_machine_learning_researcher,
+    trueflag_principal_nlp_engineer
 ]
